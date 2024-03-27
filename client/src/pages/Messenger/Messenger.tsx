d9ac9611288b0a322288b234820e1834acc39eae
VanyaR <vanya.ruzhcov@gmail.com>
2024-03-27 12:39:55 +0300

    Feat: Сделала отображение подписок в чате

--- client/src/pages/Messenger/Messenger.tsx
+++ client/src/pages/Messenger/Messenger.tsx
@@ -1,63 +1,52 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import NavBar from '../Navbar/NavBar';
 import Search from './components/Search';
 import styles from './css/Messenger.module.css';
-
-interface Message {
-  id: number;
-  text: string;
-  sender: string;
-}
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import ChatNavBar from './components/ChatNavBar';
 
 export default function Messenger(): JSX.Element {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [newMessage, setNewMessage] = useState<string>('');
-  const [isClicked, setIsClicked] = useState<string>('');
+  const subscriptions = useSelector(
+    (store: RootState) => store.auth.subscription
+  );
+  const users = useSelector((store: RootState) => store.auth.allUsers);
 
-
-  const handleSendMessage = () => {
-    if (newMessage.trim() === '') {
-      return;
-    }
-    
-    const newMessageObject: Message = {
-      id: Date.now(),
-      text: newMessage.trim(),
-      sender: 'Friend',
-    };
-
-    setMessages(prevMessages => [...prevMessages, newMessageObject]);
-    setNewMessage('');
-  };
+  const subscribedUsers = users.filter((user) =>
+    subscriptions.some((subscription) => subscription.subscribe_id === user.id)
+  );
+  console.log(subscribedUsers);
 
   return (
     <div className={styles.container}>
-        <div className={styles.title}>
-          <span className={styles.title_name}>Чат</span>
-        </div>
+      <div className={styles.title}>
+        <span className={styles.title_name}>Чат</span>
+      </div>
 
-        <Search/>
+      <Search />
 
-        <div className={styles.nav_btn_group}>
-          <button className={`${styles.trials_btn} ${isClicked === 'all'? styles.clicked : ''}`} onClick={() => setIsClicked('all')}><span>Все</span></button>
-          <button className={`${styles.trials_btn} ${isClicked === 'phy'? styles.clicked : ''}`} onClick={() => setIsClicked('phy')}><span>Physcult</span></button>
-          <button className={`${styles.trials_btn} ${isClicked === 'sky'? styles.clicked : ''}`} onClick={() => setIsClicked('sky')}><span>Лыжи</span></button> 
-          <button className={`${styles.trials_btn} ${isClicked === 'run'? styles.clicked : ''}`} onClick={() => setIsClicked('run')}><span>Бег</span></button>
-          <button className={`${styles.trials_btn} ${isClicked === 'bck'? styles.clicked : ''}`} onClick={() => setIsClicked('bck')}><span>Велосипед</span></button>
-          <button className={`${styles.trials_btn} ${isClicked === 'tre'? styles.clicked : ''}`} onClick={() => setIsClicked('tre')}><span>Тренеры</span></button>
-        </div>
+      <ChatNavBar />
 
-        <div className={styles.line}></div>
+      <div className={styles.line}></div>
 
-      
-        {messages.map(message => (
-            <div className={styles.message} key={message.id}>
-              <span className={styles.sender_name}>{message.sender}: </span>
-              <span className={styles.text_message}>{message.text}</span>
-            </div>
-        ))}
-      
-      <div className={styles.input_container}>
+      <div className={styles.chat_container}>
+        {subscribedUsers.map((chat) => {
+          return (
+            <>
+              <div className={styles.chat_box}>
+                <div className={styles.chat_avatar_img}>
+                  <img src={chat.avatar_img} alt="" />
+                </div>
+                <div
+                  className={styles.chat_name}
+                >{`${chat.name} ${chat.surname}`}</div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+
+      {/* <div className={styles.input_container}>
         <input type="text" placeholder='Новое сообщение' value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
         <button id="sendButton" onClick={handleSendMessage}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 664 663">
@@ -65,8 +54,8 @@ export default function Messenger(): JSX.Element {
             <path stroke-linejoin="round" stroke-linecap="round" stroke-width="33.67" stroke="#6c6c6c" d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888" ></path>
           </svg>
         </button> 
-      </div>
-      <NavBar/>
+      </div> */}
+      <NavBar />
     </div>
-  )
+  );
 }
